dbb34029ece51a7f343f4ba364cab2f87416921d
martinnacimiento <martinnacimiento98@gmail.com>
2021-10-19 21:45:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,289 @@
+const data = [
+    {
+        name: "Lo Siento BB:/ (with Bad Bunny & Julieta Venegas)",
+        image: "https://i.scdn.co/image/ab67616d0000b273d2dcc68851d00413147d9af6",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/1dcdeca78007b79c6cf5ffe3785f302e11b487e4?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+
+    {
+        name: "EL MUNDO ES MÍO",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/57e1150ecb0dca8d7856309dab38bd4f896e2aec?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "TE MUDASTE",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/4b2612fb7d969678d72d61908b62b73d592a737e?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "HOY COBRÉ",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/ed3eac946a786dc1bfaa89cb1d135428046c119c?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "MALDITA POBREZA",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/9bcd64fbdf300ad3b95075688aa6d97af3156bef?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "LA NOCHE DE ANOCHE",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/a98b639351290bdb69b865ad10c2e0f5dbe0c4cd?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "TE DESEO LO MEJOR",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/5734e3556195f4598b5562d9c8f0630693f51fec?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "YO VISTO ASÍ",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/7b32b29728bd3c03d3c21e0540ce549ea24caada?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "HACIENDO QUE ME AMAS",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/b9c23099bd993e55aa9ac967477c36e57e2d3a7d?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "BOOKER T",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/ddf13c4036c5fbc40c48f84cc50e46546e28d03a?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "LA DROGA",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/f5fa83512e118f224eb48d3ee08de6fb74def689?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "DÁKITI",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/edd9bea2e66b14169f8bbfa45a78b6599b14160a?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "TRELLAS",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/977f92132795b9d60164a3392b3775eaae0574d9?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "SORRY PAPI",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/5128fa764cd9dbd5e9ed2a1c38ce9de818b21eb3?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "120",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/ca4743d002a11cb4d53e459e656465ab720c83ca?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "ANTES QUE SE ACABE",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/981337ee8ac1abcd8481fde60a0bdb86962f08e1?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+    {
+        name: "CANTARES DE NAVIDAD",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/5e53b48406449353b7b526edf51b5fc2fe3f8854?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+        image: "https://i.scdn.co/image/ab67616d0000b273005ee342f4eef2cc6e8436ab",
+    },
+
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Si Veo a Tu Mamá",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/030b0d06fd4154482d5cb122b5c8c6678c86f666?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "La Difícil",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/b368a8887077ef0d389964973e2ee298f1b03948?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Pero Ya No",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/3ee13218dbfa2dee450e7c1ffdf13a5dd73f559e?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "La Santa",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/0fba2ae7b6f5588e301a6c0008914e1e864b6e66?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Yo Perreo Sola",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/3575ce15e6727094dc606c9d29e79f5830340172?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Bichiyal",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/5f4a6327e7fe5f4fc88ba9af38606fa56e6e0fe5?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Soliá",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/dbaa9f225421d9ef9c5f545683eb4d61b71dd5fd?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "La Zona",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/7016190bd267e0efc240c87daabcaa78cf87ed6a?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Que Malo",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/7760bca9133924066c295820dedcdc77f86c88ca?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Vete",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/2803e299f302d099cd11449e450d5bf21749fec5?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Ignorantes",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/3bccb33d85b30645bf6f26f80f774ee2ca06f63d?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "A Tu Merced",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/fa010f780196cdaba49c019c9153a37a57b157c6?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Una Vez",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/50b73314ddbeb49534f686bd3c7d63f5588d228b?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Safaera",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/c67f59e5d3806b41f54f772ba8cc24410ed0a258?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "25/8",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/3bc483beaaf2c1891f44f6a83b84e0967a1a32a0?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Está Cabrón Ser Yo",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/3d8d81866fc4972d8bdb40e40715fcf7f00c47fa?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Puesto Pa' Guerrial",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/959fb8f30fd0ae26ed1b201c50f69ad40e946152?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "P FKN R",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/08f9fcb7406d7eb7ac05ae21aed5759bf2763f28?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "Hablamos Mañana",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/2bffbbd1527308d0d743babc3cf85472d9e5fa30?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+    {
+        image: "https://i.scdn.co/image/ab67616d0000b273548f7ec52da7313de0c5e4a0",
+        name: "<3",
+        preview_url:
+            "https://p.scdn.co/mp3-preview/a67f999a5710d690a46eb0f1b2ce826fd5e4a679?cid=98eadcb89c044a2d8ac3924f4f55ee20",
+    },
+];
+
+const songs = data.sort((a, b) => (a.name.length < b.name.length ? 1 : -1));
+
+const song = {
+    track: 0,
+    source: null,
+};
+
+function newSong(song) {
+    const cover = document.getElementById("cover");
+    cover.setAttribute("src", song.image);
+    const title = document.getElementById("title");
+    title.textContent = song.name;
+    this.source = new Audio(song.preview_url);
+}
+
+function play() {
+    if (this.source.paused) {
+        changeIcon("pause.png");
+        this.source.play();
+    } else {
+        this.source.pause();
+        changeIcon("play.png");
+    }
+}
+
+function next() {
+    this.source.pause();
+    this.track++;
+    newSong.call(this, songs[this.track]);
+    this.source.play();
+}
+
+function back() {
+    this.source.pause();
+    this.track--;
+    newSong.call(this, songs[this.track]);
+    this.source.play();
+}
+
+newSong.call(song, songs[song.track]);
+
+const changeIcon = (icon) => {
+    const img = document.getElementById("play-icon");
+    img.setAttribute("src", icon);
+};
+
+const btn = document.getElementById("play");
+btn.addEventListener("click", (e) => {
+    play.call(song);
+});
+
+const btnNext = document.getElementById("next");
+btnNext.addEventListener("click", (e) => {
+    next.call(song);
+});
+
+const btnBack = document.getElementById("back");
+btnBack.addEventListener("click", (e) => {
+    back.call(song);
+});
+
+console.log("Ready!");
